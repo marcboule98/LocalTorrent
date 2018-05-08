3d3727b5c0e35d7295146fc348af2bc0f52c010d
MarcB <marcboule@MacBook-Pro-de-Marc.local>
2018-05-08 08:23:25 +0200

    Eliminar paginas de prueba

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'includes/vendor/autoload.php';
-use Transmission\Transmission;
-
-$transmission = new Transmission();
-$torrent = $transmission->get(1);
-
-$transmission->remove($torrent, true);
-?>
