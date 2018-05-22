eeb7381d6a12bbf86234da7a97728421bff49283
Jotxee <Jotxee@MacBook-Pro-de-Jose.local>
2018-05-23 00:23:16 +0200

    add Favicon :)
    - Añadido Favicon en el header.
    - Añadido en el directorio css logoFavicon.png y el logoLocalTorrent.png

--- includes/header.php
+++ includes/header.php
@@ -5,3 +5,5 @@
 <!-- CSS -->
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo Utils::addArchivoNoCache("css/base.css"); ?>">
+<!-- FAVICON -->
+<link rel="icon" type="image/png" href="css/logoFavicon.png" />
