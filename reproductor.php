cbbde3d092bb4dc62d7e4474d12dd41f5c79f1a0
MarcB <marcboule@MacBook-Pro-de-Marc.local>
2018-05-22 12:36:16 +0200

    Fix otra vez el reproductor

--- reproductor.php
+++ reproductor.php
@@ -7,12 +7,11 @@ if(!isset($_SESSION["idUsuario"])) {
 	throw new Exception("No se encuentra la sesion del usuario");
 } else {
 	if(!file_exists($file)){
-		echo $file;
-		//throw new Exception("Error, no se encuentra el archivo.");
+		throw new Exception("Error, no se encuentra el archivo.");
 	} else if(!is_readable($file)) {
 		throw new Exception("Error de lectura");
 	} else {
-		header('Content-Length: ' . filesize($videoFile));
+		header('Content-Length: ' . filesize($file));
 		header("Content-Type: " . $tipo);
 		readfile($file);
 	}
