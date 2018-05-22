bbd53736f37854a76cb9bc7dffd4f19afbf5a837
MarcB <marcboule@MacBook-Pro-de-Marc.local>
2018-05-22 12:30:11 +0200

    Pruebas reproductor

--- reproductor.php
+++ reproductor.php
@@ -6,9 +6,11 @@ $tipo = isset($_GET["tipo"]) ? $_GET["tipo"] : "";
 if(!isset($_SESSION["idUsuario"])) {
 	throw new Exception("No se encuentra la sesion del usuario");
 } else {
-	if(!file_exists($file) || $file === '' || !is_readable($file)){
-		header('HTTP/1.1 404 File not found', true);
-		exit;
+	if(!file_exists($file)){
+		echo $file;
+		//throw new Exception("Error, no se encuentra el archivo.");
+	} else if(!is_readable($file)) {
+		throw new Exception("Error de lectura");
 	} else {
 		header('Content-Length: ' . filesize($videoFile));
 		header("Content-Type: " . $tipo);
