58a266070a7652773267185c0edc39e506ca9d4b
MarcB <marcboule@MacBook-Pro-de-Marc.local>
2018-05-24 12:13:53 +0200

    Añadir condicion de permisos

--- lib/Controlador/ControladorAjax.php
+++ lib/Controlador/ControladorAjax.php
@@ -210,6 +210,8 @@ Class ControladorAjax extends BaseCtl {
 	private function isEspacioDisponible($url, $rutaDescargas) {
 		if(!is_dir($rutaDescargas)) {
 			throw new Exception("La ruta descargas indicada no es correcta!");
+		} else if(!is_writable($rutaDescargas) && !is_readable($rutaDescargas)) {
+			throw new Exception("La ruta no tiene permisos de escritura i/o lectura.");
 		}
 
 		$infoT = new TorrentInfo($url);
